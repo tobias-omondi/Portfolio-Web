dbffbcc77db4aedf44623a3f0ba8c4402acf1995
tobias <tobias.omondi@student.moringaschool.com>
2025-03-12 13:09:46 +0300

    created new files with folders

---
+++ my-portofolio-app/src/Componets/MainPage/MainComponets.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainComponets = () => {
+  return (
+    <div>
+      <h2>All the props and imports of all page</h2>
+    </div>
+  )
+}
+
+export default MainComponets
---
+++ my-portofolio-app/src/Componets/ServicesPage/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+      <h1>Talk about services and links to my project including articles</h1>
+    </div>
+  )
+}
+
+export default Services
