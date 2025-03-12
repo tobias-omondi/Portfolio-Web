ea55203ba1a6d2befb86d0abe0e59528d21c3b5a
tobias <tobias.omondi@student.moringaschool.com>
2025-03-12 15:45:35 +0300

    Navbar intergration

--- my-portofolio-app/src/App.jsx
+++ my-portofolio-app/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Componets/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Navbar/>
+
+      <section id="questions" style={{ height: '100vh', padding: '50px', backgroundColor: '#f4f4f4' }}>
+        <h2>Questions Section</h2>
+      </section>
+      
+      <section id="contact" style={{ height: '100vh', padding: '50px', backgroundColor: '#ddd' }}>
+        <h2>Contact Section</h2>
+      </section>
+
+      <section id="pricing" style={{ height: '100vh', padding: '50px', backgroundColor: '#ccc' }}>
+        <h2>Pricing Section</h2>
+      </section>
+    </div>
+
   )
 }
 
-export default App
+export default App;
--- my-portofolio-app/src/Componets/Navbar.jsx
+++ my-portofolio-app/src/Componets/Navbar.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import '../App.css'
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <>
+    <div className='navbar-components'>
+      <div>
+        <nav>
+        <ul>
+            <li><Link to = "questions" smooth = {true} duration={500}>Questions</Link></li>
+        </ul>
+        <ul className='contact-container'>
+            <button>
+            <li><Link to = "contact" smooth = {true} duration={500}>Contact</Link></li>
+            </button>
+        </ul>
+        <ul className='contact-pricing'>
+            <button>
+            <li><Link to='pricing' smooth = {true} duration={500}>Pricing</Link></li>
+            </button>
+        </ul>
+        </nav>
+      </div>
     </div>
+    </>
   )
 }
 
