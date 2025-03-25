3acb287f0ee7d9b8ade67e9cc127802d7cf97bfb
tobias <tobias.omondi@student.moringaschool.com>
2025-03-25 13:33:49 +0300

    question section

--- my-portofolio-app/src/Componets/ContactPage/Contact.jsx
+++ my-portofolio-app/src/Componets/ContactPage/Contact.jsx
@@ -1,13 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./Contact.css";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import Footer from '../FooterPage/Footer';
 
 const Contact = () => {
-  return (
-    <div>
-    <section id="contact" style={{ height: '100vh', padding: '50px', backgroundColor: '#ddd' }}>
-      <h2>Contact Section</h2>
-    </section>
-    </div>
-  )
-}
+  const [showForm, setShowForm] = useState(false);
 
-export default Contact
+  const handleOpenForm = () => {
+    setShowForm(true);
+  };
+
+  const handleCloseForm = () => {
+    setShowForm(false);
+  };
+
+  return (
+    <>
+    <div className='contact-container'>
+      <section id="contact">
+        <div className='contact-heading'>
+          <h2>YOUR IDEAS, BUILT WITH PURPOSE"</h2>
+          <div className='contact-heading-h4'>
+            <h4>Reach Out Today</h4>
+          </div>
+          <div className='contact-details'>
+            <div className='topic-button-book'>
+              <button type='submit' onClick={() => window.location.href = 'tel:+254 741041580'}>Call Today</button>
+            </div>
+            <div className='topic-button-share'>
+              <button type='submit' onClick={handleOpenForm}>Share Your Vision</button>
+            </div>
+          </div>
+        </div>
+
+        {showForm && (
+          <div className='form-overlay'>
+            <div className='form-container'>
+              <button onClick={handleCloseForm} className='close-button'>
+                <IoCloseCircleOutline size={25} />
+              </button>
+              <iframe
+                src='https://forms.gle/GJiq7XpjFUNRMUPW7'
+                title='Share your vision form'
+                className='iframe-form'
+              ></iframe>
+            </div>
+          </div>
+        )}
+      </section>
+    </div>
+    <Footer/>
+    </>
+  );
+};
+
+export default Contact;
---
+++ my-portofolio-app/src/Componets/FooterPage/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <h2>jhcscsh</h2>
+    </div>
+  )
+}
+
+export default Footer
