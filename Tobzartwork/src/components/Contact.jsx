d773adf61d0b8bf8df5e93ac82e4a8a7112bab42
tobias <tobias.omondi@student.moringaschool.com>
2025-10-20 11:38:34 +0300

    design cta

--- Tobzartwork/src/components/Contact.jsx
+++ Tobzartwork/src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
+    <div className='mt-20'>
       <h1 className='text-center text-6xl text-gray-50'>CONTACT</h1>
     </div>
   )
