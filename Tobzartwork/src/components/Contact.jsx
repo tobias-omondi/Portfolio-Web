4df5930c54915c8012b549ecc20c236bf9649d1f
tobias <tobias.omondi@student.moringaschool.com>
2025-10-21 20:36:26 +0300

    design work

--- Tobzartwork/src/components/Contact.jsx
+++ Tobzartwork/src/components/Contact.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import BrainScene from './BrainScene'
 
 const Contact = () => {
   return (
     <div className='mt-20'>
       <h1 className='text-center text-6xl text-gray-50'>CONTACT</h1>
+
+      <div className='flex flex-row justify-evenly items-center'>
+          <BrainScene />
+          {/* <div>
+            <h1 className='contact-design'>contact</h1>
+          </div> */}
+      </div>
     </div>
   )
 }
