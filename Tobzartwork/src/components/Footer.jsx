c3acf325f6935b91ccb188231e021d9947561c68
tobias <tobias.omondi@student.moringaschool.com>
2025-10-24 15:02:01 +0300

    changed ui componets

--- Tobzartwork/src/components/Footer.jsx
+++ Tobzartwork/src/components/Footer.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-blue-500 h-screen'>
+    <div className='bg-blue-400 h-auto underline-offset-1'>
       <div className='p-10'>
-      <h1 className='text-white text-5xl lg:text-6xl font-bold'>MY VISION</h1>
-      <p className='text-white w-full lg:w-2/3 text-xl'>To create digital experiences that feel alive—blending design, code, and creativity into work that inspires people. My vision is to grow into one of the best creative developers of my generation, building not just websites, but stories that connect. I want my journey to prove that with consistency, focus, and faith, you can transform your life and leave a mark bigger than yourself</p>
-
-      <div className='mt-5'>
-        <h2 className='text-8xl lg:text-[11rem] font-black text-start text-white'>LIVE BOLD</h2>
+  
+      <div >
+        <h5 className='text-8xl lg:text-[20rem] font-black text-center text-white'>TOBZ</h5>
       </div>
 
       </div>
