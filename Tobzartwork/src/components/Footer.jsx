50afbaaa662c5f9fce9ce01e59234cf354a15f8f
tobias <tobias.omondi@student.moringaschool.com>
2025-10-27 09:50:20 +0300

    footer file

--- Tobzartwork/src/components/Footer.jsx
+++ Tobzartwork/src/components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
 
   
   return (  
-    <div className='space-y-6 mt-10'>
+    <div className='space-y-5 mt-10 height-full'>
       <hr className="border-t border-gray-600 w-full" />
     <div className='p-10 flex flex-col md:flex-row justify-between items-center space-y-4'>
         <a href='mailto:' className='text-gray-500'>tobiasogola42@gmail.com</a>
@@ -24,7 +24,7 @@ const Footer = () => {
         </div>
     </div>
   
-      <div className='flex space-x-6 text-end justify-end items-center px-10 mt-5'>
+      <div className='flex space-x-6 text-center px-2 md:text-end justify-end items-center md:px-10 mt-5'>
         <a href='linkedin.com/in/tobias-ogola-5b4b2521b' className='text-gray-800'>LINKEDIN</a>
         <a href='linkedin.com/in/tobias-ogola-5b4b2521b' className='text-gray-800'>X</a>
         <a href='linkedin.com/in/tobias-ogola-5b4b2521b' className='text-gray-800'>INSTAGRAM</a>
@@ -32,7 +32,7 @@ const Footer = () => {
         <a href='linkedin.com/in/tobias-ogola-5b4b2521b' className='text-gray-800'>DEV.TO</a>
     </div>
     <div className='mt-10'>
-      <h5 className='text-gray-200 text-7xl md:text-[14rem] text-center font-black'>TOBZ WORK</h5>
+      <h5 className='text-gray-300 text-7xl md:text-[14rem] text-center font-black mb-0 pb-0'>TOBZ WORK</h5>
     </div>
     </div>
   )
